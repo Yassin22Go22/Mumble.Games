2bc57b74fd2c276e5f3f93ab9271f1c4e296f6fa
Yassin <yassin22game@gmail.com>
2025-02-28 14:07:29 +0200

    Add files via upload

--- scripts.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener('scroll', function() {
-    const blubImage = document.getElementById('blubImage');
-    const scrollPosition = window.scrollY;
-    blubImage.style.top = `${-90 + scrollPosition * 0.15}px`; // Adjust the multiplier as needed
-});
